a51d113b1d598a2323c89e314d9ab55058a4d089
Tunghs <juk08843@gmail.com>
2024-11-13 14:53:11 +0900

    Feat: 데이터 삭제 기능 추가
    * 현재 드라이브 용량 확인 후 오래된 순으로 파일 제거하는 기능 추가

---
+++ src/Cleaner.Util/DirectoryCleaner.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace Cleaner.Util
+{
+    public class DirectoryCleaner
+    {
+        private long _availableSpace;
+        private long _totalSpace;
+        private double _desiredFreeRatio;
+        private string _managedDirectoryPath;
+        private SearchOption _searchOption = SearchOption.TopDirectoryOnly;
+
+        public void CheckDriveStatus()
+        {
+            DriveInfo drive = new DriveInfo("C");
+            _availableSpace = drive.AvailableFreeSpace;
+            _totalSpace = drive.TotalSize;
+        }
+
+        public void SetManagedDirectoryPath(string directoryPath)
+        {
+            _managedDirectoryPath = directoryPath;
+        }
+
+        public void SetDesiredFreeRatio(double desiredFreeRatio)
+        {
+            _desiredFreeRatio = desiredFreeRatio;
+        }
+
+        public void Run()
+        {
+            Cleanup();
+        }
+
+        private void Cleanup()
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(_managedDirectoryPath, "*.*", _searchOption);
+                var filesInfo = files.Select(file => new FileInfo(file))
+                                     .OrderBy(f => f.LastWriteTime)
+                                     .ToList();
+
+                long bytesToFree = ComputeRequiredFreeSpace();
+                long freedBytes = 0;
+
+                foreach (var fileInfo in filesInfo)
+                {
+                    freedBytes += fileInfo.Length;
+                    Console.WriteLine($"Deleting: {fileInfo.FullName}, Size: {fileInfo.Length / 1024} KB");
+                    fileInfo.Delete();
+
+                    if (freedBytes >= bytesToFree)
+                    {
+                        break; // 필요한 용량 확보 시 삭제 중지
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An error occurred during cleanup: " + ex.Message);
+            }
+        }
+
+        private long ComputeRequiredFreeSpace()
+        {
+            DriveInfo drive = new DriveInfo("C");
+            return (long)((1 - _desiredFreeRatio) * _totalSpace) - drive.AvailableFreeSpace;
+        }
+    }
+}
