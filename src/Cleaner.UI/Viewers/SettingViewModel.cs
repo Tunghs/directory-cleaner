5ec73a3a3e14a8c5041adb65cfa9d618e9766cbf
Tunghs <juk08843@gmail.com>
2024-11-24 21:23:41 +0900

    Feat: UI 기능 구현
    * 파일 삭제 관련 버튼 추가 및 기능 구현

--- src/Cleaner.UI/Viewers/SettingViewModel.cs
+++ src/Cleaner.UI/Viewers/SettingViewModel.cs
@@ -11,8 +11,9 @@ namespace Cleaner.UI.Viewers
     public partial class SettingViewModel : ViewModelBase
     {
         #region Fields
-        private OpenFolderDialog _openFolderDialog = new OpenFolderDialog() { Title = "Select Directory" };
-        private DirectoryCleaner _directoryCleaner = new DirectoryCleaner();
+        private OpenFolderDialog _openFolderDialog;
+        private DirectoryCleaner _directoryCleaner;
+        private double _interval = 0;
         #endregion
 
         #region UI Variable
@@ -20,27 +21,73 @@ namespace Cleaner.UI.Viewers
         private string _directoryPath;
 
         [ObservableProperty]
-        private string _directorySize;
+        private string _directorySizeStr;
 
         [ObservableProperty]
-        private string _availableSize;
+        private string _availableSizeStr;
+
+        [ObservableProperty]
+        private long _directorySize;
+
+        [ObservableProperty]
+        private long _unavailableSize;
+
+        [ObservableProperty]
+        private double _thresholdRatio;
+
+        [ObservableProperty]
+        private string _intervalMin;
+
+        [ObservableProperty]
+        private string _setIntervalMin;
+
+        [ObservableProperty]
+        private double _setThresholdRatio;
+
+        [ObservableProperty]
+        private bool _isEnabledStartButton = true;
+
+        [ObservableProperty]
+        private bool _isEnabledStopButton;
         #endregion
+
+        public SettingViewModel()
+        {
+            _openFolderDialog = new OpenFolderDialog() { Title = "Select Directory" };
+            _directoryCleaner = new DirectoryCleaner();
+            _directoryCleaner.TaskCompleteEvent += OnTaskComplete;
+        }
 
         #region Command
         [RelayCommand]
-        public void OpenFileDialog()
+        public void OnButtonClick(string @param)
         {
-            if (_openFolderDialog.ShowDialog() == true)
+            switch (@param)
             {
-                DirectoryPath = _openFolderDialog.FolderName;
-                InitDirectory();
+                case "UpdateThresholdRatio":
+                    UpdateThresholdRatio();
+                    break;
+                case "OpenFileDialog":
+                    OpenFileDialog();
+                    break;
+                case "Start":
+                    StartCleaner();
+                    break;                     
+                case "Stop":
+                    StopCleaner();
+                    break;                
+                case "UpdateDirectoryStatus":
+                    UpdateDirectoryStatus();
+                    break;                
+                case "ApplyTimer":
+                    ApplyTimer();
+                    break;
+                case "ResetTimer":
+                    IntervalMin = "0";
+                    break;
+                default:
+                    break;
             }
-        }
-
-        [RelayCommand]
-        public void OpCleanerStart()
-        {
-            _directoryCleaner.Run();
         }
         #endregion
 
@@ -48,8 +95,7 @@ namespace Cleaner.UI.Viewers
         private void InitDirectory()
         {
             _directoryCleaner.SetManagedDirectoryPath(DirectoryPath);
-            DirectorySize = FormatBytes(_directoryCleaner.TotalSpace);
-            AvailableSize = FormatBytes(_directoryCleaner.AvailableSpace);
+            UpdateDriveStatusUI();
         }
 
         private string FormatBytes(long bytes)
@@ -66,6 +112,84 @@ namespace Cleaner.UI.Viewers
                 max /= scale;
             }
             return "0 Bytes";
+        }
+
+        private void UpdateThresholdRatio()
+        {
+            double ratio = ThresholdRatio / 100;
+            SetThresholdRatio = ThresholdRatio;
+            _directoryCleaner.SetDesiredFreeRatio(ratio);
+
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"Threshold 값 변경. {SetThresholdRatio}%");
+        }
+
+        private void OpenFileDialog()
+        {
+            if (_openFolderDialog.ShowDialog() == true)
+            {
+                DirectoryPath = _openFolderDialog.FolderName;
+                InitDirectory();
+            }
+        }
+
+        private void UpdateDirectoryStatus()
+        {
+            if (string.IsNullOrEmpty(DirectoryPath))
+            {
+                MessageBox.Show("Please select a directory.", "Select a drectory");
+                return;
+            }
+
+            _directoryCleaner.CheckDriveStatus();
+            UpdateDriveStatusUI();
+        }
+
+        private void StartCleaner()
+        {
+            if (_interval > 0)
+            {
+                _directoryCleaner.Run(_interval);
+                IsEnabledStartButton = false;
+                IsEnabledStopButton = true;
+            }
+            else
+            {
+                _directoryCleaner.Run();
+            }
+        }
+
+        private void StopCleaner()
+        {
+            var messageBoxResult = MessageBox.Show("Are you sure you want to stop the cleaner?", "Stop", MessageBoxButtons.YesNo);
+            if (messageBoxResult == DialogResult.Yes)
+            {
+                _directoryCleaner.Stop();
+                IsEnabledStartButton = true;
+                IsEnabledStopButton = false;
+            }
+        }
+
+        private void ApplyTimer()
+        {
+            IntervalMin = SetIntervalMin;
+            _interval = int.Parse(SetIntervalMin) * 60000;
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"Interval 값 변경. {IntervalMin} Min");
+        }
+
+        private void OnTaskComplete()
+        {
+            _directoryCleaner.CheckDriveStatus();
+            UpdateDriveStatusUI();
+        }
+
+        private void UpdateDriveStatusUI()
+        {
+            DirectorySize = _directoryCleaner.TotalSpace;
+            UnavailableSize = _directoryCleaner.TotalSpace - _directoryCleaner.AvailableSpace;
+            DirectorySizeStr = FormatBytes(_directoryCleaner.TotalSpace);
+            AvailableSizeStr = FormatBytes(_directoryCleaner.AvailableSpace);
+
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"Drive 상태 확인. 현재 경로: \"{DirectoryPath}\" Total: {DirectorySizeStr}, Available: {AvailableSizeStr}");
         }
         #endregion
     }
