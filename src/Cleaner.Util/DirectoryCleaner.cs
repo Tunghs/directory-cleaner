1cbe335c44de8054a026b2e77e3222053eb83e64
Tunghs <juk08843@gmail.com>
2024-11-25 23:49:01 +0900

    Feat: Logger 추가
    * Console 함수 호출 영역 Logger로 대체

--- src/Cleaner.Util/DirectoryCleaner.cs
+++ src/Cleaner.Util/DirectoryCleaner.cs
@@ -5,15 +5,18 @@ namespace Cleaner.Util
     {
         private double _desiredFreeRatio;
         private string _managedDirectoryPath;
-        private SearchOption _searchOption = SearchOption.TopDirectoryOnly;
+        private SearchOption _searchOption = SearchOption.AllDirectories;
         private System.Timers.Timer _timer;
 
         public long TotalSpace { get; private set; }
         public long AvailableSpace { get; private set; }
 
-        private void CheckDriveStatus(string path)
+        public delegate void TaskCompleteHandler();
+        public TaskCompleteHandler TaskCompleteEvent;
+
+        public void CheckDriveStatus()
         {
-            DriveInfo drive = new DriveInfo(path);
+            DriveInfo drive = new DriveInfo(Path.GetPathRoot(_managedDirectoryPath));
             AvailableSpace = drive.AvailableFreeSpace;
             TotalSpace = drive.TotalSize;
         }
@@ -21,7 +24,7 @@ namespace Cleaner.Util
         public void SetManagedDirectoryPath(string directoryPath)
         {
             _managedDirectoryPath = directoryPath;
-            CheckDriveStatus(Path.GetPathRoot(directoryPath));
+            CheckDriveStatus();
         }
 
         public void SetDesiredFreeRatio(double desiredFreeRatio)
@@ -29,13 +32,15 @@ namespace Cleaner.Util
             _desiredFreeRatio = desiredFreeRatio;
         }
 
-        public void Run()
+        public async void Run()
         {
-            Cleanup();
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"작업 시작");
+            await Task.Run(Cleanup);
         }
 
         public void Run(double time)
         {
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"타이머 작업 시작");
             _timer = new System.Timers.Timer(time);
             _timer.Elapsed += (sender, e) => Cleanup();
             _timer.Start();
@@ -43,6 +48,7 @@ namespace Cleaner.Util
 
         public void Stop()
         {
+            Logger.Instance.Print(Logger.LogLevel.INFO, $"타이머 작업 종료");
             _timer.Elapsed -= (sender, e) => Cleanup();
             _timer.Stop();
         }
@@ -51,35 +57,43 @@ namespace Cleaner.Util
         {
             try
             {
+                long bytesToFree = ComputeRequiredFreeSpace();
+                long freedBytes = 0;
+
+                if (bytesToFree <= 0)
+                {
+                    return;
+                }
+
                 string[] files = Directory.GetFiles(_managedDirectoryPath, "*.*", _searchOption);
                 var filesInfo = files.Select(file => new FileInfo(file))
                                      .OrderBy(f => f.LastWriteTime)
                                      .ToList();
 
-                long bytesToFree = ComputeRequiredFreeSpace();
-                long freedBytes = 0;
-
+                int deletedFileCount = 0;
                 foreach (var fileInfo in filesInfo)
                 {
                     freedBytes += fileInfo.Length;
-                    Console.WriteLine($"Deleting: {fileInfo.FullName}, Size: {fileInfo.Length / 1024} KB");
                     fileInfo.Delete();
-
+                    deletedFileCount++;
                     if (freedBytes >= bytesToFree)
                     {
                         break; // 필요한 용량 확보 시 삭제 중지
                     }
                 }
+
+                Logger.Instance.Print(Logger.LogLevel.INFO, $"작업 완료. {deletedFileCount} 개 파일 삭제");
+                TaskCompleteEvent?.Invoke();
             }
             catch(Exception ex)
             {
-                Console.WriteLine("An error occurred during cleanup: " + ex.Message);
+                Logger.Instance.Print(Logger.LogLevel.ERROR, "An error occurred during cleanup: " + ex.Message);
             }
         }
 
         private long ComputeRequiredFreeSpace()
         {
-            DriveInfo drive = new DriveInfo("C");
+            DriveInfo drive = new DriveInfo(Path.GetPathRoot(_managedDirectoryPath));
             return (long)((1 - _desiredFreeRatio) * TotalSpace) - drive.AvailableFreeSpace;
         }
     }
---
+++ src/Cleaner.Util/Logger.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaner.Util
+{
+    public class Logger
+    {
+        #region Data
+        public enum LogLevel
+        {
+            TRACE,  // 추적 레벨은 Debug보다 좀 더 상세한 정보를 나타냄
+            DEBUG,  // 프로그램을 디버깅하기 위한 정보 지정
+            INFO,   // 상태변경과 같은 정보성 메시지를 나타냄
+            WARN,   // 처리 가능한 문제, 향후 시스템 에러의 원인이 될 수 있는 경고성 메시지를 나타냄
+            ERROR,  // 요청을 처리하는 중 문제가 발생한 경우
+            FATAL   // 아주 심각한 에러가 발생한 상태, 시스템적으로 심각한 문제가 발생해서 어플리케이션 작동이 불가능할 경우
+        }
+
+        private struct LogData
+        {
+            public LogLevel Level;
+            public DateTime DateTime;
+            public string Message;
+
+            public LogData(LogLevel logLevel, string msg)
+            {
+                Level = logLevel;
+                DateTime = DateTime.Now;
+                Message = msg;
+            }
+        }
+        #endregion
+
+        #region Field
+        private object _LockObject = new object();
+        private string _SaveDirectoryPath = @".\Log";
+        private ConcurrentQueue<LogData> _LogProcessQueue = new ConcurrentQueue<LogData>();
+        private string _FilePath = string.Empty;
+        #endregion
+
+        private static readonly Lazy<Logger> _Instance =
+            new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get { return _Instance.Value; }
+        }
+
+        private Logger()
+        {
+            CheckSaveDirectory();
+            Task.Run(() => { WriteLogWithLockThread(); });
+        }
+
+        public void Print(LogLevel level = LogLevel.INFO, string message = null)
+        {
+            _LogProcessQueue.Enqueue(new LogData(level, message));
+            // Console.WriteLine($"{level}, {message}");
+        }
+
+        private void WriteLogWithLockThread()
+        {
+            while (true)
+            {
+                if (_LogProcessQueue.IsEmpty)
+                    continue;
+
+                LogData logData;
+                _LogProcessQueue.TryDequeue(out logData);
+
+                lock (_LockObject)
+                {
+                    WriteLog(logData.Level, logData.DateTime, logData.Message);
+                }
+            }
+        }
+
+        public void CheckSaveDirectory()
+        {
+            if (!Directory.Exists(_SaveDirectoryPath))
+                Directory.CreateDirectory(_SaveDirectoryPath);
+        }
+
+        public string GetFilePath()
+        {
+            return _FilePath;
+        }
+
+        public void SetSaveLogPath(string path)
+        {
+            _SaveDirectoryPath = path;
+            CheckSaveDirectory();
+        }
+
+        private void WriteLog(LogLevel level, DateTime dt, string msg)
+        {
+            try
+            {
+                _FilePath = $@"{_SaveDirectoryPath}\{dt.ToString("yyyyMMdd")}.log";
+                var sb = GetLogData(level, dt.ToString("yyyy-MM-dd HH:mm:ss"), msg);
+
+                using (FileStream fs = new FileStream(_FilePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(sb.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private StringBuilder GetLogData(LogLevel level, string time, string message)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(time);
+            sb.Append(" | ");
+            sb.Append(level.ToString());
+            sb.Append(" | ");
+            sb.Append(message);
+            return sb;
+        }
+    }
+}
